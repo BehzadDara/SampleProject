a9ee94227a6cefe17cdcfd5d975ee4d15dea9a2a
behzad dara <Behzad.dara.99@gmail.com>
2024-07-03 17:43:33 +0330

    sample request and responde added

--- SampleProject.API/Controllers/AuthenticationController.cs
+++ SampleProject.API/Controllers/AuthenticationController.cs
@@ -14,6 +14,7 @@ public class AuthenticationController(IMediator mediator) : BaseController
     [SwaggerOperation("دریافت توکن")]
     [SwaggerRequestExample(typeof(LoginCommand), typeof(LoginCommandExample))]
     [SwaggerResponse(StatusCodes.Status200OK, "احراز هویت با موفقیت انجام شد", typeof(string))]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(TokenExample))]
     [SwaggerResponse(StatusCodes.Status404NotFound, "کاربر یافت نشد", typeof(void))]
     public async Task<IActionResult> Login(LoginCommand request, CancellationToken cancellationToken)
     {
---
+++ SampleProject.Application/Features/Authentication/Login/TokenExample.cs
@@ -0,0 +1,11 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+
+namespace SampleProject.Application.Features.Authentication.Login;
+
+public class TokenExample : IExamplesProvider<string>
+{
+    public string GetExamples()
+    {
+        return "SampleToken";
+    }
+}
